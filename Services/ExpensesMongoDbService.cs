88930b4cf9dc5414e83973e72e37bda6fa89a62e
polmo <polmo@MAREK>
2021-03-07 19:34:12 +0100

    Create api method stubs

--- Services/ExpensesMongoDbService.cs
+++ Services/ExpensesMongoDbService.cs
@@ -18,36 +18,72 @@ namespace Expense.Services
 			db = database;
 			_accounts = db.GetCollection<Account>("Konta");
 		}
+        #region accounts
+        public IEnumerable<Account> GetAccounts(bool withExpenses)
+        {
+            return withExpenses ? _accounts.AsQueryable() : _accounts.AsQueryable().Select(acc => new Account { Id = acc.Id, Name = acc.Name });
+        }
 
-		public IEnumerable<Account> GetAccounts(bool withExpenses)
-		{
-			return withExpenses ? _accounts.AsQueryable() : _accounts.AsQueryable().Select(acc => new Account{ Id = acc.Id, Name = acc.Name });
-		}
+        public Account GetAccountById(string id, bool withExpenses)
+        {
+            throw new NotImplementedException();
+        }
 
-		public Account GetAccountByName(string name)
-		{
-			return _accounts.Find(a => a.Name.Equals(name)).FirstOrDefault();
-		}
+        public Account CreateAccount(Account account)
+        {
+            throw new NotImplementedException();
+        }
 
-		public IEnumerable<Expense> GetExpensesForAccountName(string name)
-		{
-			return GetAccountByName(name).Expenses.AsQueryable();
-		}
+        public Account ModifyAccount(Account account)
+        {
+            throw new NotImplementedException();
+        }
 
-		public IEnumerable<Expense> AddExpense(string name, Expense expense)
-		{
+        public Account DeleteAccount(string id)
+        {
+            throw new NotImplementedException();
+        }
+        #endregion
+        #region expenses
+        public Expense AddExpense(string accountId, Expense expense)
+        {
+            throw new NotImplementedException();
+        }
 
-			var filter = Builders<Account>.Filter.And(
-				Builders<Account>.Filter.Where(x => x.Name == name)
-				);
+        public Expense ModifyExpense(string accountId, Expense expense)
+        {
+            throw new NotImplementedException();
+        }
 
-			expense.Id = ObjectId.GenerateNewId();
+        public Expense DeleteExpense(string accountId, string expenseId)
+        {
+            throw new NotImplementedException();
+        }
+        #endregion
+        //public Account GetAccountByName(string name)
+        //{
+        //	return _accounts.Find(a => a.Name.Equals(name)).FirstOrDefault();
+        //}
 
-			var update = Builders<Account>.Update.Push("expenses", expense);
+        //public IEnumerable<Expense> GetExpensesForAccountName(string name)
+        //{
+        //	return GetAccountByName(name).Expenses.AsQueryable();
+        //}
 
-			_accounts.FindOneAndUpdate(filter, update);
+        //public IEnumerable<Expense> AddExpense(string name, Expense expense)
+        //{
 
-			return GetExpensesForAccountName(name);
-		}
-	}
+        //	var filter = Builders<Account>.Filter.And(
+        //		Builders<Account>.Filter.Where(x => x.Name == name)
+        //		);
+
+        //	expense.Id = ObjectId.GenerateNewId();
+
+        //	var update = Builders<Account>.Update.Push("expenses", expense);
+
+        //	_accounts.FindOneAndUpdate(filter, update);
+
+        //	return GetExpensesForAccountName(name);
+        //}
+    }
 }
