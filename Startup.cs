7cf0f17cbd05ee4e0d825df71e37e02a48a91396
marcelpolakS19133 <49458882+marcelpolakS19133@users.noreply.github.com>
2021-03-11 12:30:29 +0100

    Update Startup.cs
    Essa pushuj na proda

--- Startup.cs
+++ Startup.cs
@@ -62,12 +62,12 @@ namespace Expense
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            if (env.IsDevelopment())
-            {
+            //if (env.IsDevelopment())
+            //{
                 app.UseDeveloperExceptionPage();
                 app.UseSwagger();
                 app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Expense v1"));
-            }
+            //}
 
             app.UseRouting();
 
